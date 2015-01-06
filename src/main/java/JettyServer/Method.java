d9b1a1d508d2f7166b604f5638fffcd9153d0ae1
Konstantin Slyshkov <Konstantin.Slyshokov@gmail.com>
2015-01-06 13:06:36 +0200

    file can be defined locally

--- src/main/java/JettyServer/Method.java
+++ src/main/java/JettyServer/Method.java
@@ -2,11 +2,7 @@ package JettyServer;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 
 /**
  * Created by kot on 1/2/2015.
@@ -16,12 +12,9 @@ public class Method {
         httpServletResponse.setStatus(HttpServletResponse.SC_OK);
         String url = httpServletRequest.getRequestURI();
         FileReader file = null;
-        //fixme format java always
         try {
-            //fixme У меня нет такого пути, у меня вообще не работает :(
-            file = new FileReader("C:\\Users\\kot\\IdeaProjects\\JavaCode\\" + url.toLowerCase());
+            file = new FileReader(url.substring(1).toLowerCase());
             PrintWriter out = null;
-            //fixme не делай трай внутри трая, сделай два кетча
             BufferedReader read = new BufferedReader(file);
             String tmp = "";
             out = httpServletResponse.getWriter();
@@ -30,9 +23,7 @@ public class Method {
             }
             read.close();
         } catch (FileNotFoundException e) {
-
             httpServletResponse.sendError(410, "no such file");
-
         }
     }
 }
